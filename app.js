129ab441463bf29a96b0e1dfd035d149f34177e4
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-12 04:25:51 +0530

    Corrected to import express from 'express'

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-import express as 'express';
+import express from 'express';
 
 const app = express();
 
 app.get('/',(req,res)=>{
-    console.log("Welcome to the Subscription Tracker");
+    res.send("Welcome to the Subscription Tracker");
 })
 
 app.listen(3000,()=>{
