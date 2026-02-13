0ee19ef75a15d8566708ced257e009979638e52a
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-13 13:37:03 +0530

    Created routes for Authentication

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
 import { PORT } from './config/env.js';
 import { userRouter } from './routes/user.routes.js';
+import { subRouter } from './routes/subscription.routes.js';
+import { authRouter } from './routes/auth.routes.js';
+
 const app = express();
 
 app.get('/',(req,res)=>{
@@ -8,6 +11,9 @@ app.get('/',(req,res)=>{
 })
 
 app.use('/users',userRouter);
+app.use('/subscription', subRouter);
+app.use('/auth', authRouter);
+
 app.listen(PORT,()=>{
     console.log(`Server started on https://localhost:${PORT}`);
 })
