aa68e4d0a27bc462d4f18ac55979b3bebcf856bd
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-13 13:21:35 +0530

    Created routes folder separately instead of adding all the routes directly in app.js because it allows us to create microservices(separate functionality)

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { PORT } from './config/env.js';
+import { userRouter } from './routes/user.routes.js';
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send("Welcome to the Subscription Tracker");
 })
 
+app.use('/users',userRouter);
 app.listen(PORT,()=>{
     console.log(`Server started on https://localhost:${PORT}`);
 })
