ac444049d1f4a9c4788176900c1572cef9b11c4e
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-14 14:46:34 +0530

    Added the subscription.routes.js

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -2,10 +2,10 @@ import Router from "express";
 
 const subRouter = Router();
 
-subRouter.get("/",(req,res)=>{res.send("Get all subscriptions")});
+subRouter.get("/",(req,res)=>{res.send({title: "Get all subscriptions"})});
 subRouter.get("/:id",(req,res)=>{res.send(`Subscription route with id: ${req.params.id}`)});
 subRouter.post("/:id",(req,res)=>{res.send(`Create a new Subscription with id : ${req.params.id}`)});
 subRouter.put("/:id",(req,res)=>{res.send(`Update Subscription with id: ${req.params.id}`)});
 subRouter.delete("/:id",(req,res)=>{res.send(`Delete Subscription with id: ${req.params.id}`)});
 
-export { subRouter };
+export { subRouter };   
