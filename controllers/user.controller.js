1cce6b4bd63810013ff0531a5962ae01e2b8e6d9
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-20 20:48:50 +0530

    Authentication: Added the controller logic for getting a particular user by id

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,8 +9,23 @@ const getUsers = async (req, res, next)=>{
             error.statusCode = 404;
             throw error;
         }
-        res.status(200).json({success: true, data: users});
+        res.status(200).json({success: true, data: users}).select('-password'); // Exclude password field
 
+    }catch(error){
+        next(error);
+    }
+}
+
+const getUserById = async (req, res, next)=>{
+    try{
+        const user = await User.findById(req.params.id).select('-password'); // Exclude password field
+        
+        if(!user){
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+        res.status(200).json({success: true, data: user});
     }catch(error){
         next(error);
     }
