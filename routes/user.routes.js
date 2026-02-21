b1e68cddbce1d8aa0b1870d8ccb30be42cc363a5
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-21 13:37:22 +0530

    Created the authorization using bearer token, learn how to authorize with bearer token on postman and also created authorize.middleware.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,7 +4,7 @@ import { authorize } from "../middlewares/authorize.middleware.js";
 const userRouter = Router();
 
 userRouter.get("/",getUsers);
-userRouter.get("/:id", getUserById);
+userRouter.get("/:id",authorize, getUserById);
 userRouter.post("/:id",(req,res)=>{res.send(`Create a new User with id : ${req.params.id}`)});
 userRouter.put("/:id",(req,res)=>{res.send(`Update User with id: ${req.params.id}`)});
 userRouter.delete("/:id",(req,res)=>{res.send(`Delete User with id: ${req.params.id}`)});
