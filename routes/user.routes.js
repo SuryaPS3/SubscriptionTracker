8c6101ab6326e1fee29152e32e932f4549d0809f
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-21 12:27:29 +0530

    Fix MongoDB connection error by adding mongo service to docker-compose.yml, now it pulls the mongoDB image and runs it in the container itself

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,10 @@
 import Router from "express";
 import { getUsers, getUserById } from "../controllers/user.controller.js";
-
+import { authorize } from "../middlewares/authorize.middleware.js";
 const userRouter = Router();
 
 userRouter.get("/",getUsers);
-userRouter.get("/:id",getUserById);
+userRouter.get("/:id", getUserById);
 userRouter.post("/:id",(req,res)=>{res.send(`Create a new User with id : ${req.params.id}`)});
 userRouter.put("/:id",(req,res)=>{res.send(`Update User with id: ${req.params.id}`)});
 userRouter.delete("/:id",(req,res)=>{res.send(`Delete User with id: ${req.params.id}`)});
