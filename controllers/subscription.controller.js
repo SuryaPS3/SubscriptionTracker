545433111fc600aa9998a345d1b4c4d07f859e79
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-22 19:55:11 +0530

    added the delete subscription logic in the subscription.controller.js

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -25,3 +25,27 @@ export const getUserSubscriptions = async (req,res,next)=>{
         next(error);
     }
 }
+
+export const deleteSubscription = async (req, res, next) => {
+    try {
+        const subscription = await Subscription.findById(req.params.id);
+
+        if (!subscription) {
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        if (subscription.userId.toString() !== req.user._id.toString()) {
+            const error = new Error('You are not authorized to access this resource');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        await subscription.deleteOne();
+
+        res.status(200).json({ success: true, message: 'Subscription deleted successfully' });
+    } catch (error) {
+        next(error);
+    }
+};
