52dba40239dc9e19715aadc9de9692969a1a0072
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-23 14:50:22 +0530

    Created getSubscriptionById and it's route too

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -38,6 +38,29 @@ export const getUserSubscriptions = async (req,res,next)=>{
     }
 }
 
+export const getSubscriptionById = async (req,res,next)=>{
+    try{
+        const subscription = await Subscription.findById(req.params.id);
+
+        if(!subscription){
+            const error = new Error('Subscription not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        //check if user owns this subscriptino
+        if(subscription.userId.toString()!==req.user._id.toString()){
+            const error = new Error('You are not authorized to access this resource');
+            error.statusCode = 401;
+            throw error;
+        }
+        
+        res.status(200).json({success:true, data: subscription});
+    }catch(e){
+        next(e);
+    }
+}
+
 export const updateSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.findById(req.params.id);
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,11 +1,13 @@
 import Router from "express";
-import { createSubscription, getUserSubscriptions, updateSubscription, deleteSubscription, getAllSubscriptions } from "../controllers/subscription.controller.js";
+import { createSubscription, getUserSubscriptions, updateSubscription, deleteSubscription, 
+    getAllSubscriptions, getSubscriptionById} from "../controllers/subscription.controller.js";
 import { authorize } from "../middlewares/authorize.middleware.js";
 
 const subRouter = Router();
 
 subRouter.get("/",authorize,getAllSubscriptions);
 subRouter.get("/:id",authorize, getUserSubscriptions);
+subRouter.get("/single/:id",authorize, getSubscriptionById);
 subRouter.post("/",authorize, createSubscription);
 subRouter.put("/:id",authorize, updateSubscription);
 subRouter.delete("/:id",authorize, deleteSubscription);
