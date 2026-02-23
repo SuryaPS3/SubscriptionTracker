6df087c2669c213f1e8ae55f6602597eb6de14a4
Surya Pratap Singh <spsingh2003.sps@gmail.com>
2026-02-23 15:17:48 +0530

    Created the updateUserProfile controller and its respective route

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -30,3 +30,43 @@ export const getUserById = async (req, res, next)=>{
         next(error);
     }
 }
+
+export const updateUserProfile = async (req,res,next)=>{
+    try{
+        //check if user is updating their own profile
+        if(req.user._id.toString()!==req.params.id.toString()){
+            const error = new Error('You are not authorized to update this profile');
+            error.statusCode = 401;
+            throw error;
+        }
+
+        // remove senstivie fields from req.body
+        const {password, email, ...updateData} = req.body;
+
+        //if email update is requested, check if new email is already in use
+        if(email && email !==req.user.email){
+            const existingUser = await User.findOne({email});
+            if(existingUser){
+                const error = new Error('Email is already in use');
+                error.statusCode = 400;
+                throw error;
+            }
+            updateData.email = email; // allow email update if it's not in use
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, updateData, {new : true}).select('-password');
+
+        if(!updatedUser){
+            const error = new Error ('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
+
+        res.status(200).json({success:true,
+            message:"Profile updated successfully",
+            data : updatedUser
+        });
+    }catch(e){
+        next(e);
+    }
+}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
 import Router from "express";
-import { getUsers, getUserById } from "../controllers/user.controller.js";
+import { getUsers, getUserById , updateUserProfile} from "../controllers/user.controller.js";
 import { authorize } from "../middlewares/authorize.middleware.js";
 const userRouter = Router();
 
 userRouter.get("/",getUsers);
 userRouter.get("/:id",authorize, getUserById);
 userRouter.post("/:id",(req,res)=>{res.send(`Create a new User with id : ${req.params.id}`)});
-userRouter.put("/:id",(req,res)=>{res.send(`Update User with id: ${req.params.id}`)});
+userRouter.put("/:id",authorize, updateUserProfile);
 userRouter.delete("/:id",(req,res)=>{res.send(`Delete User with id: ${req.params.id}`)});
 
 
